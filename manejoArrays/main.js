d31fe1d3e7c80c60c097284650dd68cfa0a52906
Stella <bernardstella0704@gmail.com>
2026-03-17 10:05:05 -0300

    segunda parte del codigo

--- manejoArrays/main.js
+++ manejoArrays/main.js
@@ -1,11 +1,12 @@
 //primera parte: preparar el entorno de ingreso de datos
 
-import * as readline from "node:readline/promises"; //puerta de entrada para ingresar datos por consola. 
+import * as readline from "node:readline/promises"; //puerta de entrada para ingresar datos por consola.
 //trae las herramientas de node y las guarda en el objeto readline
 //hacemos q sea asyncrono para esperar el ingreso de datos.
 
-const rl = readline.createInterface({ //creamos la interfaz donde se leeran los datos.
-// rl es el objeto que se encarga de leer los datos ingresados por consola.
+const rl = readline.createInterface({
+  //creamos la interfaz donde se leeran los datos.
+  // rl es el objeto que se encarga de leer los datos ingresados por consola.
 
   input: process.stdin, //teclado, lo q ingresa la consola
   output: process.stdout, //pantalla, lo q se mostrara
@@ -15,3 +16,20 @@ let usuarios = []; //creo el array vacio para guardas los datos q se ingresen po
 
 //segunda parte: recoleccion de datos
 
+while (true) {
+  //ciclo infin, se repite hasta q se ejecute el break con salir
+  let nombreU = await rl.question(
+    "Ingresar nombre de usuario (O 'salir' para finalizar ingreso): ",
+  );
+  if (nombreU === "salir") break; // el break finaliza el ciclo while, no toma mas datos y pasa a la proxima parte
+  let edadU = await rl.question("Ingresas edad de usuario: ");
+  let notaU = await rl.question("Ingrese nota del usuario: ");
+
+  usuarios.push({
+    //push es un metodo para agregar elementos al final de un array, agregamos los objetos con los datos de cada ubject q serian los users
+    //clave : valor
+    nombreU: nombreU,
+    edadU: edadU,
+    notaU: notaU,
+  });
+}
